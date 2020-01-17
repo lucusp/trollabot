a99bcaa351a7eceacc97dc6a571593e9bb502852
Lucus Pettigrew <lapettigre@gmail.com>
2020-01-16 22:41:43 -0600

    Updates

---
+++ precache-manifest.14efa3022dfa80c646b6a6873d478f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f60493c7ef69750cf44f52083ce6b85",
+    "url": "/trollabot/index.html"
+  },
+  {
+    "revision": "18c772d8dbad2bfc3c52",
+    "url": "/trollabot/static/css/main.698c2ebd.chunk.css"
+  },
+  {
+    "revision": "d8f19d35112a5c0399d4",
+    "url": "/trollabot/static/js/2.a294696d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/trollabot/static/js/2.a294696d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18c772d8dbad2bfc3c52",
+    "url": "/trollabot/static/js/main.bb4910d7.chunk.js"
+  },
+  {
+    "revision": "6b6d6cae1cd874592d73",
+    "url": "/trollabot/static/js/runtime-main.865eb400.js"
+  }
+]);
